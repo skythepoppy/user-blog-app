1d677682e4473a0763f9a5dcbaae3ceb85ca4fa9
skythepoppy <raphaelsebastien.evangelista@gmail.com>
2023-08-23 12:24:56 -0500

    update admin display format

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class AdminPost(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ('status', )
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title', )}     # automatically fills in slug field with post title
 
+admin.site.register(Post, AdminPost)
 
 
