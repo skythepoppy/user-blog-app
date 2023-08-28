c0b443185489fb031e907ece56a0d9fd097fa084
skythepoppy <raphaelsebastien.evangelista@gmail.com>
2023-08-28 01:51:29 -0500

    updated wsgi file

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
-
+path = '/Users/raphaelevangelista/Desktop/blog-app/mysite/mysite'
+if path not in sys.path:
+    sys.path.insert(0, path)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
